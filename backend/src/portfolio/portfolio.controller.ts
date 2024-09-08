55fed60f96b07f9db3df0af2a0c57598d8628b4e
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-09-08 16:29:44 +0300

    feat: add portfolio swagger

--- backend/src/portfolio/portfolio.controller.ts
+++ backend/src/portfolio/portfolio.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { PortfolioService } from "./portfolio.service";
 import { CreatePortfolioDto } from "./dto/portfolio.dto";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("portfolio")
 @Controller("portfolio")
 export class PortfolioController {
   constructor(private readonly portfolioService: PortfolioService) {}
 
   @Post()
+  @ApiResponse({ status: 201, description: "Portfolio created." })
+  @ApiResponse({ status: 400, description: "Invalid data." })
   async createPortfolio(@Body() createPortfolioDto: CreatePortfolioDto) {
     return this.portfolioService.createForm(createPortfolioDto);
   }
