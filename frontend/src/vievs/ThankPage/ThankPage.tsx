9150a90cbf2230fdc3f4803a99dc2e713c06fde0
Brodich Vlad <brodich_vlad@ukr.net>
2024-10-05 16:16:48 +0300

    add animate-pulse

--- frontend/src/vievs/ThankPage/ThankPage.tsx
+++ frontend/src/vievs/ThankPage/ThankPage.tsx
@@ -6,7 +6,7 @@ import content from './content.json';
 
 export function ThankPage (): React.JSX.Element {
   return( 
-    <div className="py-[150px] px-4 min-w-[400px]" lang="uk-UA">
+    <div className="py-[150px] px-4 min-w-[320px]" lang="uk-UA">
       <div  className="w-full flex flex-col gap-11 justify-center items-center">
         <Text
           Tag="h1"
@@ -15,9 +15,9 @@ export function ThankPage (): React.JSX.Element {
           font="sans"
           align="center"
           color="base/BG_block"
-          className="font-normal w-[450px] text-2xl md:text-4xl md:w-[600px]"
+          className="font-normal max-w-[420px] text-2xl md:text-4xl md:max-w-[560px]"
         />
-        <Icon Svg={heart} width={150} height={150} />
+        <Icon className='animate-pulse' Svg={heart} width={150} height={150} />
       </div>
     </div>
   )
