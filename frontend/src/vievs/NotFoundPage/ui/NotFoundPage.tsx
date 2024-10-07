91806f278105182f5a5de9573eb701268b2bb89c
Brodich Vlad <brodich_vlad@ukr.net>
2024-10-07 12:21:38 +0300

    add NotFoundPage content

--- frontend/src/vievs/NotFoundPage/ui/NotFoundPage.tsx
+++ frontend/src/vievs/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,29 +1,38 @@
-import { Text } from "@/shared/ui/Text";
 import React from "react";
+import LogoWhit from "@/shared/assets/icons/LogoWhiteNoText.svg?react";
+import { Icon } from "@/shared/ui/Icon";
+import { ButtonLink } from "@/shared/ui/ButtonLink";
+import { Text } from "@/shared/ui/Text";
+import content from './content.json';
 
 export const NotFoundPage: React.FC = () => {
   return (
-    <div className="w-full h-full bg-base-text_accent absolute z-30 flex flex-col gap-11 items-center justify-center">
-          <Text
-            Tag="h1"
-            textType="Desktop/title-l"
-            text='404'
-            font="sans"
-            align="center"
-            color="base/text"
-            className="font-normal text-2xl md:text-7xl "
-          />
-
-          <Text
-            Tag="p"
-            textType="Desktop/H3"
-            text='Ця сторінка в розробці'
-            font="sans"
-            align="center"
-            color="base/BG_block"
-            className="font-normal text-2xl md:text-4xl "
-          />
-      <p>The page you are looking for does not exist.</p>
+    <div className="w-full h-full bg-base-text_accent fixed z-30 flex flex-col gap-7 items-center justify-center p-4">
+      <Text
+        Tag="h1"
+        textType="Desktop/title-l"
+        text={content.NotFoundPage.title}
+        font="sans"
+        align="center"
+        color="base/text"
+        className="font-normal text-7xl"
+      />
+      <div className="flex gap-6 items-baseline flex-wrap justify-center">
+        <Text
+          Tag="p"
+          textType="Desktop/H3"
+          text={content.NotFoundPage.text}
+          font="sans"
+          align="center"
+          color="base/BG_block"
+          className="font-normal text-2xl md:text-4xl "
+        />
+        <Icon Svg={LogoWhit} height={33} width={63} />
+      </div>
+      <ButtonLink 
+        variant="arrowTextBlue" 
+        text={content.NotFoundPage.btn_text} 
+        to="/" />
     </div>
   );
 };
