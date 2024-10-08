26af39bb3535d6f8d2d9d44e2c3608e58d338234
kamilka13me <125221637+kamilka13me@users.noreply.github.com>
2024-10-08 18:51:56 +0300

    fix: hotfix (#34) (#35)

--- frontend/src/vievs/MainPage/ui/OurProjects/OurProjects.tsx
+++ frontend/src/vievs/MainPage/ui/OurProjects/OurProjects.tsx
@@ -8,35 +8,38 @@ const items: BoxProps[] = [
   {
     src: UIP,
     title: 'Унікальні особини',
-    subTitle:'Ми об’єднали сили науки та мистецтва для збереження біорізноманіття українського степу.',
+    subTitle:
+      'Ми об’єднали сили науки та мистецтва для збереження біорізноманіття українського степу.',
   },
   {
     src: Cultbit,
-    title: "Cultbit: Інтелектуальна пригода у форматі AR",
-    subTitle: "Це як PokemonGo, тільки у всесвіті української культури.",
+    title: 'Cultbit: Інтелектуальна пригода у форматі AR',
+    subTitle: 'Це як PokemonGo, тільки у всесвіті української культури.',
   },
   {
-    src:Rye,
-    subTitle: "Ми сприяємо соціалізації та зниженню психічного напруження родин переселенців через реалізацію заходів культурно-мистецького спрямування.",
-    title: "Жито: проєкт соціальної адаптації внутрішньо переміщеним особам засобами мистецтва",
+    src: Rye,
+    subTitle:
+      'Ми сприяємо соціалізації та зниженню психічного напруження родин переселенців через реалізацію заходів культурно-мистецького спрямування.',
+    title:
+      'Жито: проєкт соціальної адаптації внутрішньо переміщеним особам засобами мистецтва',
   },
 ];
 
 const OurProjects = () => {
   return (
     <section className="w-full p-5">
-      <Text 
-        Tag="h2" 
-        text="Наші проекти" 
-        color="base/BG_block" 
-        textType="Desktop/H2" 
-        className="text-[36px] md:text-[48px] w-full max-w-[1128px] mx-auto mb-10"/>
-      <ul className="w-full flex flex-col gap-10">
+      <Text
+        Tag="h2"
+        text="Наші проекти"
+        color="base/BG_block"
+        textType="Desktop/H2"
+        className="mx-auto mb-10 w-full max-w-[1128px] text-[36px] md:text-[48px]"
+      />
+      <ul className="flex w-full flex-col gap-10">
         {/* box */}
-        { items.map((el,i)=>{
-            return <ProjectItem key={i} {...el}/>
-          })
-        }
+        {items.map((el, i) => {
+          return <ProjectItem key={i} {...el} />;
+        })}
       </ul>
     </section>
   );
