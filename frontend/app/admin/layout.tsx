8cea14fa1e131f9f0121e6de9540f9c4a3dad662
Brodich Vlad <brodich_vlad@ukr.net>
2024-10-09 16:51:00 +0300

    feature: create AdminLayout

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Metadata } from 'next';
+import { AdminLayout } from '@/vievs/AdminPage/AdminLayout';
 
 export const metadata: Metadata = {
   title: 'Адмін панель',
   description: 'Адмін панель сайту',
 };
 
-export default function AdminLayout(
-{children}
-: {
-  children: React.ReactNode;
-}) {
+export default function Layout(
+  {children}
+  : {
+    children: React.ReactNode;
+  }): React.JSX.Element
+ {
   return (
-    <>
+    <AdminLayout>
       {children}
-    </>
+    </AdminLayout>
   );
 }
---
+++ frontend/src/vievs/AdminPage/AdminLayout/AdminLayout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from 'react';
+import Dashboard from './ui/Dashboard';
+
+export function AdminLayout({children}
+  : {
+    children: React.ReactNode;
+  }): React.JSX.Element
+  {
+  return (
+    <div className='min-h-[90vh] flex flex-row gap-8 p-10'>
+      <div className='w-[236px] bg-base-text_dark rounded-[30px] p-6'>
+        <Dashboard/>
+        {/* <ul className='w-full flex flex-col gap-4 items-center'>
+          <li>
+            Адмін панель
+          </li>
+          <li>
+            Адмін панель
+          </li>
+          <li>
+            Адмін панель
+          </li>
+        </ul> */}
+      </div>
+      {children}
+    </div>
+  )
+}
