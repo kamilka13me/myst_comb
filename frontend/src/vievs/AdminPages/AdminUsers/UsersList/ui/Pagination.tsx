60308f93b79d92ed8bfd1ba7253985f0de631838
Brodich Vlad <brodich_vlad@ukr.net>
2024-10-15 11:31:11 +0300

    feat: add functions Pagination

--- frontend/src/vievs/AdminPages/AdminUsers/UsersList/ui/Pagination.tsx
+++ frontend/src/vievs/AdminPages/AdminUsers/UsersList/ui/Pagination.tsx
@@ -1,24 +1,48 @@
-import { Icon } from "@/shared/ui/Icon";
+import { Icon } from '@/shared/ui/Icon';
 import ArrowInCircleBlue from '@/shared/assets/icons/ArrowInCircleBlue.svg?react';
-import { Text } from "@/shared/ui/Text";
+import { Text } from '@/shared/ui/Text';
+import { useState } from 'react';
+import clsx from 'clsx';
 
-export default function Pagination(): JSX.Element{
+export default function Pagination(): JSX.Element {
+  const [pageNum, setPageNum] = useState<number>(1);
+
+  const prevPage = (): void => {
+    if (pageNum > 1) {
+      setPageNum(pageNum - 1);
+    }
+  };
+
+  const nextPage = (): void => {
+    if (pageNum < 10) {
+      setPageNum(pageNum + 1);
+    }
+  };
+
   return (
-    <div className="w-full flex items-center justify-between">
-      <button type="button" className="duration-300 hover:opacity-70">
-        <Icon
-          Svg={ArrowInCircleBlue}
-          width={28}
-          height={28}
-          className="min-w-[28px] rotate-[-135deg]"
-        />
-      </button>
-
+    <div className="flex w-full items-center justify-between">
+      <div>
+        <button
+          type="button"
+          className={clsx(
+            'duration-300 hover:opacity-70',
+            pageNum < 2 && 'hidden',
+          )}
+          onClick={prevPage}
+        >
+          <Icon
+            Svg={ArrowInCircleBlue}
+            width={28}
+            height={28}
+            className="min-w-[28px] rotate-[-135deg]"
+          />
+        </button>
+      </div>
       <div className="flex items-center justify-center gap-0.5">
         <Text
           Tag="span"
           textType="Desktop/Subtitle"
-          text={'2'}
+          text={pageNum.toString()}
           align="center"
           font="sans"
           color="base/text"
@@ -44,7 +68,11 @@ export default function Pagination(): JSX.Element{
         />
       </div>
 
-      <button type="button" className="duration-300 hover:opacity-70">
+      <button
+        type="button"
+        className="duration-300 hover:opacity-70"
+        onClick={nextPage}
+      >
         <Icon
           Svg={ArrowInCircleBlue}
           width={48}
@@ -53,5 +81,5 @@ export default function Pagination(): JSX.Element{
         />
       </button>
     </div>
-  )
+  );
 }
