6afdc4b5ad5f6598c193b97d8ab3d9de3d686c3c
kamilka13me <kamilka13me@gmail.com>
2025-06-30 09:54:47 +0300

    feat: upd ip

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'http://alicesocial.pp.ua:5000/api/:path*',
       },
     ];
   },
